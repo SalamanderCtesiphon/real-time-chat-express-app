6897cb2869b1447f73c39fbbad113b5cec6190a7
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-28 14:23:24 -0500

    depricated

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ const server = app.listen(3000, () => {
 app.use(express.static(__dirname));
 
 
-var Message = mongoose.model(‘Message’,{ name : String, message : String})
+var Message = mongoose.model("Message",{ name : String, message : String});
 
 mongoose.connect(dbUrl);
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  });
+});
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    res.sendStatus(200);
+  })
+});
