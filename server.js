5dacbb9e977ad00cefa15c0350969559a2c1da0e
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-28 13:40:23 -0500

    created and implemented the data base for the app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbUrl = 'mongodb+srv://swbrookshire:database@cluster0.0zulids.mongodb.net/?retryWrites=true&w=majority';
 
 const server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port);
