fd184866aa10eb880f0b1eaca71bdca7362224f6
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-28 13:32:27 -0500

    working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const server = app.listen(3000, () => {
-  console.log('server is running on port', server.address().port());
+  console.log('server is running on port', server.address().port);
 });
 
 app.use(express.static(__dirname));
